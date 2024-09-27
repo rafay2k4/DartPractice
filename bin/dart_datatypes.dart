c96fc904507cec5798ea301f7285239f9127b676
rafay0000 <itsrafay03@gmail.com>
2024-09-28 03:34:07 +0500

    Further New Changes.

--- bin/dart_datatypes.dart
+++ bin/dart_datatypes.dart
@@ -2,4 +2,13 @@ void main(List<String> arguments) {
   print('Hello Pakistan.');
   var a = 2;
   print(a + 5);
+
+  var s = SchoolFriend(name: 'Ali', rollNo: 23);
+}
+
+class SchoolFriend {
+  String? name;
+  int? rollNo;
+
+  SchoolFriend({required this.name, required this.rollNo});
 }
